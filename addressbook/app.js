06378a88a6b7a722b8722a8b8025c2ca4df28c89
Pedro Carara <cararax@gmail.com>
2021-04-26 08:09:14 -0300

    adicionando rota no app.js pra startar a aplicacao

--- addressbook/app.js
+++ addressbook/app.js
@@ -6,7 +6,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var personsRouter = require('./routes/persons');
+
 var app = express();
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/persons', personsRouter);
 
 module.exports = app;
